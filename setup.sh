5327c589cd2203b8d04845fec3083024ebd9a971
liuyixin-louis <zxcxzcz@qq.com>
2022-05-03 12:04:22 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 # update apt source
 cd $(cd $(dirname $0);pwd)
-pip install apt-select
-apt-select
+#pip install apt-select
+#apt-select
 cp sources.list /etc/apt/sources.list
-apt update
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential
 # config zsh
 # apt install zsh
 sudo apt install zsh -y
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://hub.fastgit.xyz/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 # cp .oh-my-zsh  ~/.oh-my-zsh -r
 cp .zshrc ~/.zshrc # my config
 sudo apt-get install powerline fonts-powerline
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://hub.fastgit.xyz/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 chsh -s /bin/zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://hub.fastgit.xyz/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://hub.fastgit.xyz/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 # vscode setting
 cp settings.json /root/.local/share/code-server/User/settings.json
